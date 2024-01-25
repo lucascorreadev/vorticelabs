c013505e7ed66058bdc8f5b60ed560b56b5b0034
Correa <fclucascorrea@gmail.com>
2024-01-25 19:41:46 -0300

    [feat] implements main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'vortice-background': "url('./images/background-vortice.png)",
+      },
+    },
+    fontFamily:{
+      sans: [
+        'Century Gothic', 'Arial', 'sans-serif'
+      ]
+    }
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+        const newUtilities = {
+            ".no-scrollbar::-webkit-scrollbar": {
+                display: "none",
+            },
+            ".no-scrollbar": {
+                "-ms-overflow-style": "none",
+                "scrollbar-width": "none",
+            },
+        };
+        addUtilities(newUtilities);
+    },
+],
 }
